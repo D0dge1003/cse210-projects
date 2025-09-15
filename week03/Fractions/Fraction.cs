f113cc1620e3fad0fd30c2b1d0ac3db75f3adb73
d0dge1003 <ignaciokarelearl@gmail.com>
2025-09-15 10:50:00 +0800

    Fraction Activity

---
+++ week03/Fractions/Fraction.cs
@@ -0,0 +1,51 @@
+using System;
+using System.Formats.Asn1;
+
+public class Fraction
+{
+    public int _top;
+    public int _bottom;
+
+    public Fraction()
+    {
+        _top = 1;
+        _bottom = 1;
+    }
+    public Fraction(int wholeNumber)
+    {
+        _top = wholeNumber;
+        _bottom = 1;
+    }
+    public Fraction(int top, int bottom)
+    {
+        _top = top;
+        _bottom = bottom;
+    }
+
+    public int GetTop()
+    {
+        return _top;
+    }
+
+    public void SetTop(int top)
+    {
+        _top = top;
+    }
+    public int GetBottom()
+    {
+        return _bottom;
+    }
+    public void SetBottom(int bottom)
+    {
+        _bottom = bottom;
+    }
+    public string GetFractionString()
+    {
+        return $"{_top}/{_bottom}";
+    }
+    public double GetDecimalValue()
+    {
+        double answer = (double)_top / _bottom;
+        return answer;
+    }
+}
